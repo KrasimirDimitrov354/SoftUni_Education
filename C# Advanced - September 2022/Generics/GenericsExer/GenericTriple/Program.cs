ab674dcd4651efc3763877dc9e4b4f77428f541c
Krasimir Dimitrov <krasidimitrov354@gmail.com>
2022-10-17 18:15:15 +0300

    Update 17.10.2022

---
+++ C# Advanced - September 2022/Generics/GenericsExer/GenericTriple/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace GenericTriple
+{
+    //Threeuple
+    //Create a Class Threeuple. It must hold three objects with their own getters and setters.
+    //
+    //Input
+    //The input consists of three lines:
+    //  •	The first one is holding a name, an address and a town. Format of the input:
+    //      {first name} {last name} {address} {town}
+    //      Note: The town name can be comprised of more than one word.
+    //
+    //  •	The second line is holding a name, beer liters and a boolean variable with values "drunk" or "not". Format:
+    //      {name} {liters of beer} {boolean variable}
+    //
+    //  •	The last line will hold a name, a bank balance (double) and a bank name. Format:
+    //      {name} {account balance} {bank name}
+    //Output
+    //  •	Print the Threeuples' objects in format:
+    //      "{firstElement} -> {secondElement} -> {thirdElement}"
+    //
+    //Examples
+    //Input
+    //  Adam Smith Wallstreet New York
+    //  Mark 18 drunk
+    //  Karren 0.10 USBank
+    //Output
+    //  Adam Smith -> Wallstreet -> New York
+    //  Mark -> 18 -> True
+    //  Karren -> 0.1 -> USBank
+    //Input
+    //  Anatoly Andreevich Kutuzova Kaliningrad
+    //  Marley 9 not
+    //  Grant 2 NGB
+    //Output
+    //  Anatoly Andreevich -> Kutuzova -> Kaliningrad
+    //  Marley -> 9 -> False
+    //  Grant -> 2 -> NGB
+
+    public class Program
+    {
+        static void Main()
+        {
+            string[] input = GetInput();
+            Threeuple<string, string, string> firstTriple = new Threeuple<string, string, string>(
+                String.Join(' ', input, 0, 2),
+                input[2],
+                String.Join(' ', input, 3, input.Length - 3));
+
+            input = GetInput();
+            Threeuple<string, int, bool> secondTriple = new Threeuple<string, int, bool>(
+                input[0],
+                int.Parse(input[1]),
+                GetBool(input[2]));
+
+            input = GetInput();
+            Threeuple<string, double, string> thirdTriple = new Threeuple<string, double, string>(
+                input[0],
+                double.Parse(input[1]),
+                input[2]);
+
+            Console.WriteLine(firstTriple.ToString());
+            Console.WriteLine(secondTriple.ToString());
+            Console.WriteLine(thirdTriple.ToString());
+        }
+
+        private static bool GetBool(string input)
+        {
+            if (input == "drunk")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string[] GetInput()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ C# Advanced - September 2022/Generics/GenericsExer/GenericTuple/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GenericTuple
+{
+    //Tuple
+    //Implement a variant of the C# class Tuple that can hold two objects and print them in the format {item1} -> { item2}.
+    //
+    //Input
+    //The input consists of three lines:
+    //  •	The first one is holding a person's name and an address. They are separated by space(s).
+    //      Your task is to collect them in the tuple and print them on the console. Format of the input:
+    //      {first name} {last name} {address}
+    //
+    //  •	The second line holds a name of a person and the amount of beer (int) he can drink. Format:
+    //      {name} {liters of beer}
+    //
+    //  •	The last line will hold an integer and a double. Format:
+    //      {integer} {double}
+    //Output
+    //  •	Print the tuples’ items in format: {item1} -> {item2}
+    //Constraints
+    //  •	Create the class and make it have getters and setters for its class variables.
+    //  •	The input will always be valid, no need to check it explicitly.
+    //
+    //Examples
+    //Input                     Output
+    //Adam Smith California     Adam Smith -> California
+    //Mark 2                    Mark -> 2
+    //23 21.23212321            23 -> 21.23212321
+    //
+    //Input                     Output
+    //William Donovan York      William Donovan -> York
+    //Richard 2999999           Richard -> 2999999
+    //10 10                     10 -> 10
+
+    public class Program
+    {
+        static void Main()
+        {
+            string[] input = GetInput();
+            Tuple<string, string> firstTuple = new Tuple<string, string>(String.Join(' ', input, 0, 2), input[2]);
+
+            input = GetInput();
+            Tuple<string, int> secondTuple = new Tuple<string, int>(input[0], int.Parse(input[1]));
+
+            input = GetInput();
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+
+            Console.WriteLine(firstTuple.ToString());
+            Console.WriteLine(secondTuple.ToString());
+            Console.WriteLine(thirdTuple.ToString());
+        }
+
+        public static string[] GetInput()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
